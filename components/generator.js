59900759da2783be74c936530791b9e8f279eea5
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-12-23 21:50:24 -0500

    Add files via upload

--- components/generator.js
+++ components/generator.js
@@ -85,7 +85,7 @@
 
     function generateAvatars() { 
         var avatarBookmarkList = AvatarBookmarks.getBookmarks();
-        print("GEN-VERSION-110");
+        print("GEN-VERSION-120");
         var avatars = [];
         var i = 0;
         var avatar;

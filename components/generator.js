6688ddee5bdb1ae2984eefc022638959445ae445
Alezia Kurdis <60075796+AleziaKurdis@users.noreply.github.com>
2021-12-23 22:46:04 -0500

    Add files via upload

--- components/generator.js
+++ components/generator.js
@@ -39,6 +39,7 @@
     var positionZero;
     var avatarModelEntityIDs = [];
     var avatarModelLoaded = [];
+    var avatarModelScale = [];
     var processAvatar = 0;
     var stillSomeToLoad = false;
     
@@ -89,7 +90,7 @@
         
     function generateAvatars() { 
         var avatarBookmarkList = AvatarBookmarks.getBookmarks();
-        print("GEN-VERSION-180");
+        print("GEN-VERSION-190");
         var avatars = [];
         var i = 0;
         var avatar;
@@ -135,6 +136,7 @@
             
             avatarModelEntityIDs.push(id);
             avatarModelLoaded.push(false);
+            avatarModelScale.push(avatars[i].scale);
 
         }
         
@@ -150,7 +152,7 @@
                 var properties = Entities.getEntityProperties(avatarModelEntityIDs[processAvatar], ["naturalDimensions"]);
                 
                 Entities.editEntity(avatarModelEntityIDs[processAvatar], {
-                    "dimensions": Vec3.multiply( avatars[i].scale, properties.naturalDimensions ),
+                    "dimensions": Vec3.multiply(avatarModelScale[processAvatar].scale, properties.naturalDimensions ),
                     "locked": true,
                     "visible": true
                 }); 
